bd1f2ffbcd3f208c8a81af96fd016d1e40100e9e
bojadlabalaji <bojadlabalaji@gmail.com>
2024-02-24 18:17:48 -0500

    initial commit

---
+++ src/views/SearchView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="search-main">
+      <h1>This is Your Landing page</h1>
+    </div>
+  </template>
+  
+  <style>
+  @media (min-width: 1024px) {
+    .search-main {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+  </style>
+  
